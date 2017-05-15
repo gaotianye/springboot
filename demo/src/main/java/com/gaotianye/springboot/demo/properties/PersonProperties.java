1b3f2ae577a55a29dae83f22d2a458a9ddad6f9b
gaotianye <15156506755@163.com>
2017-05-15 13:15:57 +0800

    add：配置文件的实体类

---
+++ demo/src/main/java/com/gaotianye/springboot/demo/properties/PersonProperties.java
@@ -0,0 +1,39 @@
+package com.gaotianye.springboot.demo.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="Person")
+
+public class PersonProperties {
+	private String username;
+	private String password;
+	private int age;
+	private String address;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
