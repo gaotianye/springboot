18b0d76f20925ddf9ea80f5631d5db8e867ce4fc
gaotianye <15156506755@163.com>
2017-05-15 17:13:50 +0800

    update:getAll测试

--- demo/src/main/java/com/gaotianye/springboot/demo/controller/TestController.java
+++ demo/src/main/java/com/gaotianye/springboot/demo/controller/TestController.java
@@ -1,18 +1,25 @@
 package com.gaotianye.springboot.demo.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gaotianye.springboot.demo.properties.PersonProperties;
+import com.gaotianye.springboot.demo.repository.Person;
+import com.gaotianye.springboot.demo.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/testController")
 public class TestController {
 	@Resource
 	private PersonProperties person;
+	@Resource
+	private PersonRepository repository;
 	
 	@RequestMapping(value = "/test1",method = RequestMethod.GET)
 	public String test1(){
@@ -21,5 +28,10 @@ public class TestController {
 		int age = person.getAge();
 		String address = person.getAddress();
 		return "username : "+username+",password : "+password+",age : "+age+",address : "+address;
+	}
+	
+	@GetMapping(value="/getAll")
+	public List<Person> getList(){
+		return repository.findAll();
 	}
 }
